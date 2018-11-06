efe9c42b9ed536d4a647da583fd5548d93b38f13
GrzLo <251583@uwr.edu.pl>
2018-11-06 14:31:28 +0100

    quadratic equation - interface

---
+++ QuadraticEquation.h
@@ -0,0 +1,14 @@
+
+class QuadraticEquation {
+private:
+	double _a;
+	double _b;
+	double _c;
+
+public:
+	QuadraticEquation(double a, double b, double c);
+	
+	double Roots(unsigned int which_root);
+};
+
+
